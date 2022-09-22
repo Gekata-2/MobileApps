1ca3c56f5e6c4306a472f307595d5af816dfdcd1
Gekata-2 <sosorii2002@gmail.com>
2022-09-22 20:57:28 +0300

    added clickable image

---
+++ lab1.1/app/src/main/java/com/example/learning/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClicked(View v) {
+        TextView txt = findViewById(R.id.text);
+        txt.setText(String.valueOf(++counter));
+    }
+}
